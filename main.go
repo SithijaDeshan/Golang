044b23788993a59b0da62c7cda318f6836d4adc7
SithijaDeshan <cst20075@std.uwu.ac.lk>
2026-05-17 21:36:06 +0530

    Refactor booking application to include conference details and user input for ticket booking

--- main.go
+++ main.go
@@ -3,6 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to out conference booking application")
+	conferenceName := "DK Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n",conferenceTickets,remainingTickets)
 	fmt.Println("Get your ticket here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name : ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name : ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email : ")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter how many tickets do you want : ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receeive a confirmation email at %v.", firstName,lastName,userTickets,email)
+
+
+	
 }
