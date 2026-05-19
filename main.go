cb564f2a4984a9807e086c041f70568c5b75c241
SithijaDeshan <cst20075@std.uwu.ac.lk>
2026-05-19 22:37:12 +0530

    Refactor main.go for improved readability and validation checks in ticket booking process

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "DK Conference"
@@ -8,36 +11,56 @@ func main() {
 	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n",conferenceTickets,remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var bookings []string
+	bookings := []string{}
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Please enter your first name : ")
-	fmt.Scan(&firstName)
+		fmt.Println("Please enter your first name : ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your last name : ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please enter your last name : ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter your email : ")
-	fmt.Scan(&email)
+		fmt.Println("Please enter your email : ")
+		fmt.Scan(&email)
 
-	fmt.Println("Please enter how many tickets do you want : ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Please enter how many tickets do you want : ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isEmailValid := strings.Contains(email, "@")
+		isValidTecketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receeive a confirmation email at %v.\n", firstName,lastName,userTickets,email)
-	fmt.Printf("%v tickets remaining for %v.\n",remainingTickets,conferenceName)
+		if isValidName && isEmailValid && isValidTecketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receeive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
 
-	
+			fmt.Printf("The first names of bookings are : %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tickets remaining, So you cant book %v tickets.\n", remainingTickets, userTickets)
+		}
+
+	}
+
 }
